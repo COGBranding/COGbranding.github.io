20c4464911d92452dd8829285b09040993f02a97
COG Branding <81595974+COGBranding@users.noreply.github.com>
2022-06-06 10:21:48 +1000

    Update diviCart.js

--- js/diviCart.js
+++ js/diviCart.js
@@ -7,16 +7,10 @@ div.classList.add("menu-item", "account-icon");
 let accountIcon = '<a href="/my-account"></a>';
 div.innerHTML = accountIcon;
 
-function menuCart() {
+function menuCart(site_width) {
     // For tablets and smaller devices, add the account and cart icons
-    if (window.innerWidth <= 980) {
-        document
-            .getElementById("et_mobile_nav_menu")
-            .prepend(
-                document.getElementsByClassName("menu-cart-info")
-                    .parent()
-                    .html()
-            ) + document.getElementsByClassName("menu-cart-info").before(div);
+    if (window.innerWidth <= site_width) {
+        document.getElementById("et_mobile_nav_menu").prepend(document.getElementsByClassName("menu-cart-info").parentNode.html()) + document.getElementsByClassName("menu-cart-info").before(div);
     } else {
         // Add the account icon to the end of the desktop menu
         let lastMenuItem = document.getElementById("top-menu").lastChild;
