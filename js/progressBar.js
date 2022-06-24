60bccc059bfbd6eaabf552e8bf5eae01006c48bf
Prabhat Giri <prabhat.giri@cogdigital.com.au>
2022-06-24 12:04:55 +1000

    fix

--- js/progressBar.js
+++ js/progressBar.js
@@ -1,8 +1,6 @@
-//create a element with any id that you want to add the progress bar to.
-//pass the element id to this function and it will handle the rest
-
-function progressBar(selector_id){
-	const scrollline = document.querySelector('#'+ selector_id);
+//create a element with  id reading-progress.
+function progressBar(){
+	const scrollline = document.querySelector('#reading-progress');
 
       function fillscrollline(){
         const windowHeight = window.innerHeight;
@@ -16,16 +14,14 @@ function progressBar(selector_id){
       window.addEventListener('scroll', fillscrollline);
 }
 
-//create a element with any class to create a follower.
-//pass the element class to this function and it will handle the rest
-
+//create a element with  class "mouse-follow-circle" to create a follower.
 function mouse_follower(selecter_class){
 
     $(document).on("click mousemove","body",function(e){ 
         var x = e.clientX;
         var y = e.clientY;
         var newposX = x ;
-        var newposY = y; $("." + selecter_class ).css("transform","translate3d("+newposX+"px,"+newposY+"px,0px)");
+        var newposY = y; $(".mouse-follow-circle" ).css("transform","translate3d("+newposX+"px,"+newposY+"px,0px)");
     });
 
 }
