2146b6838cfb32ffff9ea36821338a31fec82053
COG Branding <81595974+COGBranding@users.noreply.github.com>
2022-06-30 17:11:04 +1000

    Update diviFunction.js

--- js/diviFunction.js
+++ js/diviFunction.js
@@ -116,13 +116,6 @@ function mouseFollowArrow(){
 }
 
 function footerCollapse() {
-    if (window.innerWidth <= 980) {
-        // console.log(content.style.maxHeight);
-        content.style.maxHeight = '0px';
-        content.style.overflow = 'hidden';
-        content.style.transition = '0.3s';
-    }
-
     var acc = document.getElementsByClassName("footer__accordion");
     var i;
 
@@ -132,6 +125,13 @@ function footerCollapse() {
 
             var content = document.getElementById("footer__content");
             console.log(content);
+
+            if (window.innerWidth <= 980) {
+                // console.log(content.style.maxHeight);
+            content.style.maxHeight = '0px';
+            content.style.overflow = 'hidden';
+            content.style.transition = '0.3s';
+        }
 
             if (window.innerWidth <= 980) {
                 // console.log(content.style.maxHeight);
