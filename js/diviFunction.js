490f039046affa219dfc740096106be7bd986367
prabhat giri <prabhat.giri@cogdigital.com.au>
2022-07-26 13:54:13 +1000

    added .fd-c-m on diviNormalize.css

--- js/diviFunction.js
+++ js/diviFunction.js
@@ -105,9 +105,9 @@ function mouseFollowArrow(){
             el != null;
             lx += el.offsetLeft, ly += el.offsetTop, el = el.offsetParent);
         if (pos === 'x'){
-        return lx
+            return lx
         } else {
-        return ly
+            return ly
         }
     }
 
@@ -142,20 +142,20 @@ function footerCollapse() {
 function accordionClose(){
     jQuery(function($){
         $('.et_pb_toggle_title').click(function(){
-        var $toggle = $(this).closest('.et_pb_toggle');
-        if (!$toggle.hasClass('et_pb_accordion_toggling')) {
-            var $accordion = $toggle.closest('.et_pb_accordion');
-            if ($toggle.hasClass('et_pb_toggle_open')) {
-            $accordion.addClass('et_pb_accordion_toggling');
-            $toggle.find('.et_pb_toggle_content').slideToggle(700, function() { 
-                $toggle.removeClass('et_pb_toggle_open').addClass('et_pb_toggle_close'); 
-                        
-            });
+            var $toggle = $(this).closest('.et_pb_toggle');
+            if (!$toggle.hasClass('et_pb_accordion_toggling')) {
+                var $accordion = $toggle.closest('.et_pb_accordion');
+                if ($toggle.hasClass('et_pb_toggle_open')) {
+                $accordion.addClass('et_pb_accordion_toggling');
+                $toggle.find('.et_pb_toggle_content').slideToggle(700, function() { 
+                    $toggle.removeClass('et_pb_toggle_open').addClass('et_pb_toggle_close'); 
+                            
+                });
+                }
+                setTimeout(function(){ 
+                $accordion.removeClass('et_pb_accordion_toggling'); 
+                }, 750);
             }
-            setTimeout(function(){ 
-            $accordion.removeClass('et_pb_accordion_toggling'); 
-            }, 750);
-        }
         });
     });
 }
