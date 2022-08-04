b451ca743fccc6ad27a6cdb923c1d245d7830092
COGbranding <cogbranding.digital@gmail.com>
2022-08-04 11:17:15 +1000

    Removed menuDiviCart function

--- js/diviCart.js
+++ js/diviCart.js
@@ -7,24 +7,24 @@ div.classList.add("menu-item", "account-icon");
 let accountIcon = '<a href="/my-account"></a>';
 div.innerHTML = accountIcon;
 
-
 //menuCart(980, 1, 67715231);
-function menuCart(site_width, loadPhone='', phone_num='') {
+function menuCart(site_width, loadPhone = "", phone_num = "") {
     // For tablets and smaller devices, add the account and cart icons
     if (window.innerWidth <= site_width) {
-        $("#et_mobile_nav_menu").prepend($(".menu-cart-info").parent().html()) + $(".menu-cart-info").before(div);
+        $("#et_mobile_nav_menu").prepend($(".menu-cart-info").parent().html()) +
+            $(".menu-cart-info").before(div);
     } else {
         // Add the account icon to the end of the desktop menu
         let lastMenuItem = document.getElementById("top-menu").lastChild;
         lastMenuItem.classList.add("myAccount");
         lastMenuItem.after(div);
     }
-    if(loadPhone){
+    if (loadPhone) {
         menuPhone(phone_num);
     }
 }
 
-// Load Cart Icon Only
-function menuDiviCart() {
-    $("#et_mobile_nav_menu").prepend($(".menu-cart-info").parent().html()) + $(".menu-cart-info").before(div);
-}
+// // Load Cart Icon Only
+// function menuDiviCart() {
+//     $("#et_mobile_nav_menu").prepend($(".menu-cart-info").parent().html()) + $(".menu-cart-info").before(div);
+// }
