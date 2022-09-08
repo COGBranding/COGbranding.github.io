773d747d15d786fbf0c5767453498a2bfbc64822
Raman Bhadel <raw.man.353@gmail.com>
2022-09-09 09:35:16 +1000

    Added js to add an overlay over the main content which is shown when mobile menu is opened. And CSS to the added div id

--- js/diviFunction.js
+++ js/diviFunction.js
@@ -1,12 +1,14 @@
 console.log("diviFunction is working");
 
+
+
 // Current Year HTML
 function currentYear() {
     document.getElementById("year").innerHTML = new Date().getFullYear();
 }
 
-// open all external links and pdfs on new tabs
 $(document).ready(function (e) {
+    // open all external links and pdfs on new tabs
     $("a").each(function () {
         var a = new RegExp("/" + window.location.host + "/");
         if (!a.test(this.href)) {
@@ -18,6 +20,13 @@ $(document).ready(function (e) {
         }
         $('a[href$=".pdf"]').attr("target", "_blank");
     });
+
+    //Adds dark background overlay on divi mobile menu open 
+    var html = '<div id="cog-overlay"></div>';
+    $(html).insertBefore('#main-content')
+    $('#et_mobile_nav_menu .mobile_menu_bar_toggle').click(function(){
+        $('#cog-overlay').toggle()
+    })
 });
 
 // Phone Icon in Menu
