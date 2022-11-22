4f7b7c43f3f7ee065a6e496fc1be80b09afb042b
Blake <contact@blakezajac.com>
2022-11-22 14:25:19 +1100

    Add landing-page.js

---
+++ cog-branding/js/landing-page.js
@@ -0,0 +1,7 @@
+// Dropdown menu
+var dropdown = document.querySelector(".dropdown__field");
+var dropdownItems = document.querySelector(".dropdown__items__list");
+
+dropdown.addEventListener("click", function () {
+    dropdownItems.classList.toggle("closed");
+});
