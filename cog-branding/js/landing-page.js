01fd16ff94296dff5302d8c5570fed0202d637fb
Blake <contact@blakezajac.com>
2022-11-23 16:38:55 +1100

    Update landing-page.js

--- cog-branding/js/landing-page.js
+++ cog-branding/js/landing-page.js
@@ -1,55 +1,57 @@
 // Dropdown menu
-function dropdownMenu() {
-    var dropdown = document.querySelector(".dropdown__field");
-    var dropdownItems = document.querySelector(".dropdown__items__list");
+document.addEventListener("DOMContentLoaded", (event) => {
+    function dropdownMenu() {
+        var dropdown = document.querySelector(".dropdown__field");
+        var dropdownItems = document.querySelector(".dropdown__items__list");
+
+        if (dropdown) {
+            dropdown.addEventListener("click", function (event) {
+                dropdownItems.classList.toggle("open");
+                // event.stopPropagation();
+            });
+
+            window.onclick = function (event) {
+                if (!event.target.matches(".dropdown")) {
+                    if (dropdownItems.classList.contains("open")) {
+                        dropdownItems.classList.remove("open");
+                    }
+                }
+            };
+
+            dropdown.addEventListener("click", function (event) {
+                event.stopPropagation();
+            });
+        }
+
+        // Alternate method
+        // window.addEventListener("click", function (elem) {
+        //     if (elem.path[0].classList.contains("open")) {
+        //         elem.stopPropagation();
+        //     } else if (elem.path[0].classList.contains("dropdown__field")) {
+        //         dropdownItems.classList.remove("closed");
+        //         dropdownItems.classList.add("open");
+        //     } else {
+        //         dropdownItems.classList.add("closed");
+        //         dropdownItems.classList.remove("open");
+        //     }
+        // });
+    }
 
     if (dropdown) {
-        dropdown.addEventListener("click", function (event) {
-            dropdownItems.classList.toggle("open");
-            // event.stopPropagation();
-        });
-
-        window.onclick = function (event) {
-            if (!event.target.matches(".dropdown")) {
-                if (dropdownItems.classList.contains("open")) {
-                    dropdownItems.classList.remove("open");
-                }
-            }
-        };
-
-        dropdown.addEventListener("click", function (event) {
-            event.stopPropagation();
-        });
+        dropdownMenu();
     }
 
-    // Alternate method
-    // window.addEventListener("click", function (elem) {
-    //     if (elem.path[0].classList.contains("open")) {
-    //         elem.stopPropagation();
-    //     } else if (elem.path[0].classList.contains("dropdown__field")) {
-    //         dropdownItems.classList.remove("closed");
-    //         dropdownItems.classList.add("open");
-    //     } else {
-    //         dropdownItems.classList.add("closed");
-    //         dropdownItems.classList.remove("open");
-    //     }
-    // });
-}
+    function updatePostMoreText() {
+        var postButton = document.querySelector(".sec-blog__posts .g1-button");
 
-if (dropdown) {
-    dropdownMenu();
-}
+        if (postButton) {
+            for (let i = 0; i < postButton.clientHeight; i++) {
+                postButton.innerHTML = "Read more";
+            }
+        }
 
-function updatePostMoreText() {
-    var postButton = document.querySelector(".sec-blog__posts .g1-button");
-
-    if (postButton) {
-        for (let i = 0; i < postButton.clientHeight; i++) {
-            postButton.innerHTML = "Read more";
+        if (postButton) {
+            updatePostMoreText();
         }
     }
-
-    if (postButton) {
-        updatePostMoreText();
-    }
-}
+});
